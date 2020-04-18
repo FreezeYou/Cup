a4ebd68dc71fd75daaf865cd9df05c6488462e31
Playhi <000902play@gmail.com>
2020-04-18 15:44:05 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm782925225-72029296f1167aef80b98730', //Zidon.Net
-	'm784771794-c2913ad3136d9381c68f2239', //2.zidon.Net
-	'm781771575-0f8fe29af858bb6b284f7ac0', //FreezeYou.Playhi.Net
+	'm782925225-72029296f1167aef80b98730', // Zidon.Net
+	'm784771794-c2913ad3136d9381c68f2239', // 2.zidon.Net
+	'm784771809-8ae37d64350f65e482ea3f67', // Status.zidon.Net
+	'm781771575-0f8fe29af858bb6b284f7ac0', // FreezeYou.Playhi.Net
 ];
 
 // refresh interval (in seconds)
